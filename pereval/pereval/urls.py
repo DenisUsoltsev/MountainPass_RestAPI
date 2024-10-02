618bd996b3afc32f36aba544dacf404075295836
Денис <straigaldvir@yandex.ru>
2024-10-02 09:08:52 +0400

    Changes to the file urls.py
    Добавил путь для представления SubmitDataView

--- pereval/pereval/urls.py
+++ pereval/pereval/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from mountain_pass.views import SubmitDataView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', SubmitDataView.as_view(), name='submit_data'),
 ]

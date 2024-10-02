616589896106b574593008ce17e5d66089c5404b
Денис <straigaldvir@yandex.ru>
2024-10-02 12:30:20 +0400

    Debugging
    Добавил проверку, чтобы посмотреть какое поле вызывает ошибку.

--- pereval/mountain_pass/serializers.py
+++ pereval/mountain_pass/serializers.py
@@ -32,12 +32,26 @@ class PerevalAddedSerializer(serializers.ModelSerializer):
         model = PerevalAdded
         fields = ['beauty_title', 'title', 'other_titles', 'connect', 'add_time', 'user', 'coords', 'images', 'level']
 
+    # Отладка - проверка какое поле вызывает ошибку
+    def validate(self, data):
+        # Отладка типа данных
+        if not isinstance(data.get('user'), dict):
+            raise serializers.ValidationError(f"user: Ожидался dictionary, получен {type(data.get('user'))}")
+        if not isinstance(data.get('coords'), dict):
+            raise serializers.ValidationError(f"coords: Ожидался dictionary, получен {type(data.get('coords'))}")
+        if not isinstance(data.get('images'), list):
+            raise serializers.ValidationError(f"images: Ожидался list, получен {type(data.get('images'))}")
+        if not isinstance(data.get('level'), dict):
+            raise serializers.ValidationError(f"level: Ожидался dictionary, получен {type(data.get('level'))}")
+
+        return data
+
     def create(self, validated_data):
         # Получение и удаление вложенных данных
         user_data = validated_data.pop('user')
         coords_data = validated_data.pop('coords')
         level_data = validated_data.pop('level')
-        # images_data = validated_data.pop('images')
+        images_data = validated_data.pop('images')
 
         # Добавляем пользователя
         user = User.objects.create(email=user_data['email'], defaults=user_data)
@@ -54,5 +68,9 @@ class PerevalAddedSerializer(serializers.ModelSerializer):
         pereval_added.autumn = level_data.get('autumn', '')
         pereval_added.spring = level_data.get('spring', '')
         pereval_added.save()
+
+        # Добавляем изображения
+        for image in images_data:
+            PerevalImage.objects.create(pereval=pereval_added, img_path=image['data'])
 
         return pereval_added
